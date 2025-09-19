2411c67f4921fcaa92c9344632ff810b9487f1a1
sstelmaj <seba.stelmaj21@gmail.com>
2025-09-19 18:25:54 -0300

    aniadiendo vercel a CORS

--- Controlador/src/server.js
+++ Controlador/src/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'https://mecanografia-juego.vercel.app/', 
+  origin: 'https://mecanografia-juego.vercel.app', 
   methods: ['GET', 'POST'],
   credentials: true,
 }));
